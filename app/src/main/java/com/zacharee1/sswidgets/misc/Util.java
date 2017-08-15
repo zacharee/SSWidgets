bc71b410dcc585437ac61025d332d28589834a58
Zachary Wander <zachary@techyteen.tk>
2017-08-15 09:24:36 -0400

    Remove music widget

--- app/src/main/java/com/zacharee1/sswidgets/misc/Util.java
+++ app/src/main/java/com/zacharee1/sswidgets/misc/Util.java
@@ -29,49 +29,6 @@ import java.util.TreeMap;
 
 public class Util
 {
-    public static Bitmap drawableToBitmap (Drawable drawable) {
-        Bitmap bitmap = null;
-
-        if (drawable instanceof BitmapDrawable) {
-            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
-            if(bitmapDrawable.getBitmap() != null) {
-                return bitmapDrawable.getBitmap();
-            }
-        }
-
-        if(drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
-            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
-        } else {
-            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        }
-
-        Canvas canvas = new Canvas(bitmap);
-        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-        drawable.draw(canvas);
-        return bitmap;
-    }
-
-    /**
-     * Open app by specified package name/ID
-     * @param context caller's context
-     * @param packageName desired app's package name
-     * @return success
-     */
-    public static boolean openApp(Context context, String packageName) {
-        PackageManager manager = context.getPackageManager();
-        try {
-            Intent i = manager.getLaunchIntentForPackage(packageName);
-            if (i == null) {
-                return false;
-                //throw new PackageManager.NameNotFoundException();
-            }
-            i.addCategory(Intent.CATEGORY_LAUNCHER);
-            context.startActivity(i);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
 
     /**
      * Get contact image by ID
--- app/src/main/java/com/zacharee1/sswidgets/misc/Values.java
+++ app/src/main/java/com/zacharee1/sswidgets/misc/Values.java
@@ -2,15 +2,9 @@ package com.zacharee1.sswidgets.misc;
 
 public class Values
 {
-    public static final String MUSIC_INTENT_ACTION = "music_action";
     public static final String CONTACT_INTENT_ACTION = "contact_action";
     public static final String ACTION_INFORMATION_ADDED = "xyz.mustardcorp.secondscreen.action.INFO_ADDED";
     public static final String ACTION_NOTIFICATIONS_RECOMPILED = "xyz.mustardcorp.secondscreen.action.NOTIFS_REDONE";
-
-    public static final int MUSIC_BACK = -1;
-    public static final int MUSIC_PLAYPAUSE = 0;
-    public static final int MUSIC_NEXT = 1;
-    public static final int MUSIC_OPEN = 2;
 
     public static final int CONTACT_1 = 3;
     public static final int CONTACT_2 = 4;
